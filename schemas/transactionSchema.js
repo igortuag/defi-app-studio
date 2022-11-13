0032d056bcca0f5fa2ea4483c7d7b12fcbe5b781
Tuag <igortuag@gmail.com>
2022-11-13 05:22:42 -0300

    add more fields to transaction schema

--- schemas/transactionSchema.js
+++ schemas/transactionSchema.js
@@ -8,5 +8,20 @@ export const transactionSchema = {
       title: 'Transaction Hash',
       type: 'string',
     },
+    {
+      name: 'fromAddress',
+      title: 'From Address',
+      type: 'string',
+    },
+    {
+      name: 'toAddress',
+      title: 'To Address',
+      type: 'string',
+    },
+    {
+      name: 'amount',
+      title: 'Amount',
+      type: 'number',
+    },
   ],
 }
