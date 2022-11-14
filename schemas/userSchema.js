832a32683b278a51dab7de07d8e1b9f7804b09d7
Tuag <igortuag@gmail.com>
2022-11-14 08:57:41 -0300

    add user schema fields

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,5 +1,28 @@
 export const userSchema = {
-  name: 'user',
-  title: 'User',
-  type: 'document',
-}
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "password",
+      title: "Password",
+      type: "string",
+    },
+    {
+      name: "transactions",
+      title: "Transactions",
+      type: "array",
+      of: [{ type: "reference", to: { type: "transaction" } }],
+    },
+  ],
+};
